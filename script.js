2a1ef7658d0c89f793e627173477f7ab9dacdab4
nosirov123 <nosirovbilol92@gmail.com>
2025-06-30 11:54:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,264 @@
+const data = [
+    {
+      Title: "Patton Oswalt: Annihilation",
+      "fulltitle": "Patton Oswalt: Annihilation (2017)",
+      movie_year: 2017,
+      Categories: "Uncategorized",
+      "summary": "Patton Oswald, despite a personal tragedy, produces his best standup yet. Focusing on the tribulations of the Trump era and life after the loss of a loved one, Patton Oswald continues his journey to contribute joy to the world.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Patton-Oswalt-Annihilation-Movie-Poster.jpg",
+      "imdb_id": "tt7026230",
+      "imdb_rating": 7.4,
+      "runtime": 66,
+      "language": "English",
+      "ytid": "4hZi5QaMBFc"
+    },
+    {
+      Title: "New York Doll",
+      "fulltitle": "New York Doll (2005)",
+      movie_year: 2005,
+      Categories: "Documentary|Music",
+      "summary": "A recovering alcoholic and recently converted Mormon, Arthur \"Killer\" Kane, of the rock band The New York Dolls, is given a chance at reuniting with his band after 30 years.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/New-York-Doll-Movie-Poster.jpg",
+      "imdb_id": "tt0436629",
+      "imdb_rating": 7.9,
+      "runtime": 75,
+      "language": "English",
+      "ytid": "jwD04NsnLLg"
+    },
+    {
+      Title: "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "fulltitle": "Mickey's Magical Christmas: Snowed in at the House of Mouse (2001)",
+      movie_year: 2001,
+      Categories: "Adventure|Animation",
+      "summary": "After everyone is snowed in at the House of Mouse, Mickey suggests they throw their own Christmas party. Everyone is happy, except for Donald who just isn't in to the Christmas spirit. So Mickey shows a series of cartoons that show just what Christmas is all about. It features a star studded cast of Disney characters from everyone's favorite animated Disney movies.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Mickeys-Magical-Christmas-Snowed-in-at-the-House-of-Mouse-Movie-Poster.jpg",
+      "imdb_id": "tt0300195",
+      "imdb_rating": 6.8,
+      "runtime": 65,
+      "language": "English",
+      "ytid": "uCKwHHftrU4"
+    },
+    {
+      Title: "Mickey's House of Villains",
+      "fulltitle": "Mickey's House of Villains (2001)",
+      movie_year: 2001,
+      Categories: "Animation|Comedy",
+      "summary": "The villains from the popular animated Disney films are gathered at the House of Mouse with plans to take over. Soon, the villains take over the house and kick out Mickey, Donald and Goofy. It's all up to Mickey and his friends to overthrow evil and return the House of Mouse to normal--or as close to normal as it get's.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Mickeys-House-of-Villains-Movie-Poster.jpg",
+      "imdb_id": "tt0329374",
+      "imdb_rating": 6.6,
+      "runtime": 0,
+      "language": "English",
+      "ytid": "JA03ciYt-Ek"
+    },
+    {
+      Title: "And Then I Go",
+      "fulltitle": "And Then I Go (2017)",
+      movie_year: 2017,
+      Categories: "Drama",
+      "summary": "In the cruel world of junior high, Edwin suffers in a state of anxiety and alienation alongside his only friend, Flake. Misunderstood by their families and demoralized at school daily, their fury simmers quietly until an idea for vengeance offers them a terrifying release. Based on the acclaimed novel \"Project X\" by Jim Shepard, this unflinching look at adolescence explores how the powerful bonds of childhood friendship and search for belonging can become a matter of life or death.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/And-Then-I-Go-Movie-Poster.jpg",
+      "imdb_id": "tt2018111",
+      "imdb_rating": 7.6,
+      "runtime": 99,
+      "language": "English",
+      "ytid": "8CdIiD6-iF0"
+    },
+    {
+      Title: "An Extremely Goofy Movie",
+      "fulltitle": "An Extremely Goofy Movie (2000)",
+      movie_year: 2000,
+      Categories: "Animation|Comedy",
+      "summary": "It's a big time in Max's life. He's college bound with his friends and finally free of his embarrassing father as he strives to be a top contender for the X-Games. Unfortunately, Goofy loses his job and learns that he cannot get another job without a college degree. To his son's mortification, Goofy decides to join him in his campus to get that degree. Desperate to distract his father, Max talks him into joining the competing Gamma Fraternity team and introduces him to a wonderful librarian who shares his nostalgic love for 1970's pastimes. Unfortunately, things do not go according to plan as events put this father-son relationship to the test.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/An-Extremely-Goofy-Movie-Movie-Poster.jpg",
+      "imdb_id": "tt0208185",
+      "imdb_rating": 6.4,
+      "runtime": 79,
+      "language": "English",
+      "ytid": "H8oSvldAGfg"
+    },
+    {
+      Title: "Peter Rabbit",
+      "fulltitle": "Peter Rabbit (2018)",
+      movie_year: 2018,
+      Categories: "Adventure|Animation",
+      "summary": "Based on the books by Beatrix Potter: Peter Rabbit (James Corden;) his three sisters: Flopsy (Margot Robbie,) Mopsy (Elizabeth Debicki) and Cotton Tail (Daisy Ridley) and their cousin Benjamin (Colin Moody) enjoy their days harassing Mr McGregor in his vegetable garden. Until one day he dies and no one can stop them roaming across his house and lands for a full day or so. However, when one of Mr McGregor's relatives inherits the house and goes to check it out, he finds much more than he bargained for. What ensues, is a battle of wills between the new Mr McGregor and the rabbits. But when he starts to fall in love with Bea (Rose Byrne,) a real lover of all nature, his feelings towards them begin to change. But is it too late?",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Peter-Rabbit-Movie-Poster.jpg",
+      "imdb_id": "tt5117670",
+      "imdb_rating": 6.6,
+      "runtime": 95,
+      "language": "English",
+      "ytid": "7Pa_Weidt08"
+    },
+    {
+      Title: "Love Songs",
+      "fulltitle": "Love Songs (2007)",
+      movie_year: 2007,
+      Categories: "Uncategorized",
+      "summary": "Julie's boyfriend Ismaël lives with her; rather than worry about the time he spends with his colleague Alice, Julie invites Alice to join them. The three walk the streets of Paris, party, read, and sleep together. Sometimes it's lighthearted, sometimes there are jealousies. Then death strikes. In various ways, those left come to terms with the departure and absence of a loved one: showing concern, eating together, attempting new relationships, trying to \"be there\" for the other. Then, the spirit returns and new commitments are possible. The romantic elements of musical comedy play in contrast to the ambivalence of the lyrics and the story.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Love-Songs-Movie-Poster.jpg",
+      "imdb_id": "tt0996605",
+      "imdb_rating": 7.2,
+      "runtime": 91,
+      "language": "English",
+      "ytid": "s54vpKAFmS0"
+    },
+    {
+      Title: 'Deadpool',
+      "fulltitle": "89 (2017)",
+      movie_year: 2017,
+      Categories: "Uncategorized",
+      "summary": "89 tells the incredible story of one of football's greatest triumphs: when against all odds Arsenal snatched the Championship title from Liverpool at Anfield in the last minute of the last game of the 1988/89 season. It's a universal tale of a band of brothers who, led by a charismatic and deeply respected manager, came together to defy the odds and create history. Mixing archive and previously unseen footage with revealing interviews, insights and memories from the original squad, game officials, famous fans and the people who were there on the night this is the definitive account of a watershed moment in football and a must-watch for any sports fan.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/89-Movie-Poster.jpg",
+      "imdb_id": "tt7614404",
+      "imdb_rating": 8.1,
+      "runtime": 91,
+      "language": "English",
+      "ytid": "5hfAExhHTMM"
+    },
+    {
+      Title: "The Foster Boy",
+      "fulltitle": "The Foster Boy (2011)",
+      movie_year: 2011,
+      Categories: "Drama",
+      "summary": "The illegitimate orphan child, 12-year-old Max, is sold by the local minister for a basket of food to the Bösiger family, who own a mountain farm. Max' initial hope of finally finding a loving home is brutally shattered: The farmer and his wife treat Max like livestock, and their son Jacob humiliates and abuses him. Only the local teacher notices the child suffering on the farm.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/The-Foster-Boy-Movie-Poster.jpg",
+      "imdb_id": "tt2057931",
+      "imdb_rating": 7.4,
+      "runtime": 107,
+      "language": "English",
+      "ytid": "E9Qv_XVJ-js"
+    },
+    {
+      Title: "Forever My Girl",
+      "fulltitle": "Forever My Girl (2018)",
+      movie_year: 2018,
+      Categories: "Drama|Music|Romance",
+      "summary": "After being gone for a decade a country star returns home to the love he left behind.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Forever-My-Girl-Movie-Poster.jpg",
+      "imdb_id": "tt4103724",
+      "imdb_rating": 6.4,
+      "runtime": 108,
+      "language": "English",
+      "ytid": "3vqcMr1q5Uc"
+    },
+    {
+      Title: "Tom Segura: Disgraceful",
+      "fulltitle": "Tom Segura: Disgraceful (2018)",
+      movie_year: 2018,
+      Categories: "Comedy|Documentary",
+      "summary": "Comedian Tom Segura rants about funny things about pop culture and the way of living in 2018.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Tom-Segura-Disgraceful-Movie-Poster.jpg",
+      "imdb_id": "tt7379330",
+      "imdb_rating": 7.5,
+      "runtime": 0,
+      "language": "English",
+      "ytid": "kYYINJM3lPA"
+    },
+    {
+      Title: "The Secret Rules of Modern Living: Algorithms",
+      "fulltitle": "The Secret Rules of Modern Living: Algorithms (2015)",
+      movie_year: 2015,
+      Categories: "Documentary",
+      "summary": "Professor Marcus du Sautoy demystifies the hidden world of algorithms and reveals where these 2,000-year-old problem solvers came from, how they work, and what they have achieved.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/The-Secret-Rules-of-Modern-Living-Algorithms-Movie-Poster.jpg",
+      "imdb_id": "tt5818010",
+      "imdb_rating": 7.5,
+      "runtime": 58,
+      "language": "English",
+      "ytid": "hpyxmT6KSvw"
+    },
+    {
+      Title: "Secrets in the Fall",
+      "fulltitle": "Secrets in the Fall (2015)",
+      movie_year: 2015,
+      Categories: "Family",
+      "summary": "Nothing is impossible for a group of young campers, friends and enemies alike, on a weekend retreat at Camp Pinnacle. But what starts as an exciting wilderness adventure turns into trouble when a young camper goes missing and the rest of the group must set out to find him. The campers are forced to overcome their differences and band together to re-claim their weekend and make it out of the woods before dark!",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Secrets-in-the-Fall-Movie-Poster.jpg",
+      "imdb_id": "tt2522908",
+      "imdb_rating": 7.3,
+      "runtime": 0,
+      "language": "English",
+      "ytid": "eMVw1pHVMAw"
+    },
+    {
+      Title: "Silent Night",
+      "fulltitle": "Silent Night (2017)",
+      movie_year: 2017,
+      Categories: "Comedy|Drama",
+      "summary": "Adam unexpectedly visits his family house at Christmas after a few years of working abroad. No family member knows about his secret plans and the real reasons of his visit.",
+      "ImageURL": "https://hydramovies.com/wp-content/uploads/2018/04/Silent-Night-Movie-Poster.jpg",
+      "imdb_id": "tt7133554",
+      "imdb_rating": 7.5,
+      "runtime": 100,
+      "language": "English",
+      "ytid": "cA6BUYVkQoE"
+    },
+];
+
+const container = document.getElementById('container');
+const select = document.getElementById('select');
+const input = document.getElementById('input');
+let btn = document.getElementById('button');
+
+
+function showMovies(data) {
+  container.innerHTML = '';
+
+  data.forEach(movie => {
+    const card = document.createElement('div');
+
+    card.classList.add(
+      "w-[244px]",         // width barqaror
+      "h-[520px]",         // height barqaror
+      "bg-[#333333]",
+      "rounded-lg",
+      "p-3",
+      "flex",
+      "flex-col",
+      "items-center",
+      "text-center",
+      "shadow-lg",
+      "overflow-hidden"   // ortiqcha matn chiqmasligi uchun
+    );
+
+    card.innerHTML = `
+      <img src="image copy.png" alt="${movie.Title}" class="w-full h-[300px] object-cover rounded-lg" />
+      <h1 class="font-bold text-white text-lg mt-2 line-clamp-2">${movie.Title}</h1>
+      <h2 class="text-base font-normal text-white mt-2">
+        ${movie.imdb_rating || "?"} · ${movie.movie_year || "?"} · ${movie.runtime ? Math.floor(movie.runtime / 60) + "hr" + (movie.runtime % 60) + "min" : "?"}
+      </h2>
+      <span class="text-white font-normal text-sm mt-2 line-clamp-2">${movie.Categories || "Unknown"}</span>
+      <button class="w-[90px] h-[35px] flex justify-center items-center text-white text-sm font-medium bg-[#5CB85C] rounded-sm mt-auto hover:bg-green-600 transition-all cursor-pointer">
+        More info
+      </button>
+    `;
+
+    container.appendChild(card);
+  });
+}
+showMovies(data);
+
+btn.addEventListener("click", () => {
+  // console.log(value);
+  
+  let inpValue = input.value.toLowerCase();
+  let filteredCards = data.filter((value) =>
+    value.Title.toLowerCase().includes(inpValue)
+  );
+
+  showMovies(filteredCards);
+});
+
+
+select.addEventListener("change", (e) => {
+  let sorted = [...data];
+
+  if (e.target.value == "A-Z") {
+    sorted.sort((a, b) => a.Title.localeCompare(b.Title));
+    showMovies(sorted);
+  } else if (e.target.value == "Z-A") {
+    sorted.sort((a, b) => b.Title.localeCompare(a.Title));
+    showMovies(sorted);
+  }
+});
